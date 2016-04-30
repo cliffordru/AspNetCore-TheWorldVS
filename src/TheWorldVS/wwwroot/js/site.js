a6076ea0127c6c0012a43385632168dfdf8544bb
Cliff G <clifford.gray@gmail.com>
2016-04-29 23:10:46 -0400

    Use annonymous function to not pollute global scope JS

--- src/TheWorldVS/wwwroot/js/site.js
+++ src/TheWorldVS/wwwroot/js/site.js
@@ -1,3 +1,17 @@
 // site.js
-var ele = document.getElementById("username");
-ele.innerHTML = "Clifford...";
+
+// Wrap in self executing annonymous function so we do not pollute global scope
+(function (){
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Clifford...";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function() {
+        main.style = "background-color: #888;";
+    };
+
+    main.onmouseleave = function() {
+        main.style = "";
+    };
+})();
+
