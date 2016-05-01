b790b7f0f38bc1001c9d5ca3be99773c675d3448
Cliff G <clifford.gray@gmail.com>
2016-05-01 12:46:09 -0400

    jQuery example of wrapped set and using convention of $ to start variables

--- src/TheWorldVS/wwwroot/js/site.js
+++ src/TheWorldVS/wwwroot/js/site.js
@@ -1,17 +1,27 @@
 // site.js
 
 // Wrap in self executing annonymous function so we do not pollute global scope
-(function (){
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Clifford...";
+(function () {
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function() {
-        main.style = "background-color: #888;";
-    };
+    //var ele = $("#username");
+    //ele.text("Clifford...");
 
-    main.onmouseleave = function() {
-        main.style = "";
-    };
+    //var main = $("#main");
+    //main.on("mouseenter", function () {        
+    //    main.style = "background-color: #888;";
+    //});
+
+    //main.on("mouseleave", function() {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click",
+    //    function () {
+    //        var me = $(this);
+    //        alert(me.text());
+    //    });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper"); //wrapped set; convention to name jquery variable with $
 })();
 
